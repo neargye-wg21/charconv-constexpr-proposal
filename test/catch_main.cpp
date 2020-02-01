90ad861c89e6578f8726036e282125f48b4f8f9e
int3 <smertig_zhyk@mail.ru>
2020-02-01 17:21:43 +0300

    move CATCH_CONFIG_MAIN to a separate file

--- test/catch_main.cpp
+++ test/catch_main.cpp
@@ -20,41 +20,5 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
-
-#include <charconv.hpp>
-
-namespace proposal = nstd;
-
-#include <array>
-#include <cstring>
-#include <iterator>
-
-TEST_CASE("[to_chars] int") {
-  auto test = []() constexpr -> bool {
-    std::array<char, 10> str = {};
-    if (auto [p, ec] = proposal::to_chars(str.data(), str.data() + str.size(), 42); ec == std::errc{}) {
-      return str[0] == '4' && str[1] == '2';
-    }
-    return false;
-  };
-
-  constexpr auto test_to_chars_int = test();
-  static_assert(test_to_chars_int);
-  REQUIRE(test());
-}
-
-TEST_CASE("[from_chars] int") {
-  auto test = []() constexpr -> bool {
-    std::array<char, 10> str{"42"};
-    int result = -1;
-    if (auto [p, ec] = proposal::from_chars(str.data(), str.data()+str.size(), result); ec == std::errc{}) {
-      return result == 42;
-    }
-    return false;
-  };
-
-  constexpr auto test_from_chars_int = test();
-  static_assert(test_from_chars_int);
-  REQUIRE(test());
-}
